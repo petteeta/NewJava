7299807ecfe4ecdaf8be86515fe2096ecad99e50
petteeta <31806434+petteeta@users.noreply.github.com>
2017-09-30 14:00:46 -0600

    First Changes

--- app/src/main/java/com/glacierwebcreative/justjava/MainActivity.java
+++ app/src/main/java/com/glacierwebcreative/justjava/MainActivity.java
@@ -1,4 +1,4 @@
-// package com.glacierwebcreative.justjava; *******************************
+// packages com.glacierwebcreative.justjava; *******************************
 
 
 package com.glacierwebcreative.justjava;
